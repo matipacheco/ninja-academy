11b5d1ab894b5d617e61546cbffbd27d5def08c7
Mati <matipacheco@gmail.com>
2019-04-07 14:09:35 -0400

    First commit

---
+++ objects.js
@@ -0,0 +1,50 @@
+var naruto = {
+  name:     'Naruto',
+  lastName: 'Uzumaki',
+  age:      12,
+  weight:   40,
+  height:   145
+};
+
+var sakura = {
+  name:     'Sakura',
+  lastName: 'Hanuro',
+  age:      12,
+  weight:   35,
+  height:   148
+};
+
+var sasuke = {
+  name:     'Sasuke',
+  lastName: 'Uchiha',
+  age:      12,
+  weight:   42,
+  height:   150
+};
+
+var kakashi = {
+  name:     'Kakashi',
+  lastName: 'Hatake',
+  age:      26,
+  weight:   67,
+  height:   181
+};
+
+var team = {
+  name:     'Seventh team',
+  sensei:   kakashi,
+  students: [ naruto, sakura, sasuke ]
+};
+
+console.log(team);
+
+/*
+var ninja = new Object();
+
+ninja.name      = 'Naruto';
+ninja.lastName  = 'Uzumaki';
+ninja.aldea     = 'Konoha';
+ninja.age       = 12;
+ninja.weight    = 40;
+ninja.height    = 145;
+*/
