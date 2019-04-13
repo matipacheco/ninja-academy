a5d8111fb101cf9ce3a06d389854cdf81b7e5e54
Mati <matipacheco@gmail.com>
2019-04-13 15:15:34 -0400

    wrapping up Objects lesson

--- README.md
+++ README.md
@@ -156,7 +156,66 @@ Each object above represents a ninja. Each ninja has it own name, age, weight an
 
 ### Initializing objects
 
-There are different ways to define an object.
+An object doesn't necessarily need to be created following the structure shown above. There are other ways to define an object. Here are a couple more ways to do it!
+
+    let ninja = new Object();
+    
+    ninja.name      = 'Naruto';
+    ninja.lastName  = 'Uzumaki';
+    ninja.age       = 12;
+    ninja.weight    = 40;
+    ninja.height    = 145;
+    
+    console.log(ninja);
+    
+    function Ninja(name, lastName, age, weight, height) {
+      this.name     = name;
+      this.lastName = lastName;
+      this.age      = age;
+      this.weight   = weight;
+      this.height   = height
+    }
+    
+    ninja = new Ninja('Naruto', 'Uzumaki', 12, 40, 145);
+    
+    console.log(ninja);
+
+### Working with objects
+
+As we said before, the advantage of using an object, is to encapsulate a certain group of properties that are unique from the rest. We can access each and one of these properties like this:
+    
+     let age      = ninja.age;
+     let name     = ninja.name;
+     let lastName = ninja.lastName;
+     
+     console.log("Our ninja is " + age + " year old, and his name is " + name + " " + lastName);
+
+And we don't have to settle with the properties that the object initially has. We can dynamically add more fields to an object upon our code execution!
+
+    ninja.village = "Konoha";
+    ninja.sensei  = kakashi;
+    
+    console.log(ninja);
+
+    /*
+        {
+            name: 'Naruto',
+            lastName: 'Uzumaki',
+            age: 12,
+            weight: 40,
+            height: 145,
+            village: 'Konoha',
+            sensei: {
+                name: 'Kakashi',
+                lastName: 'Hatake',
+                age: 26,
+                weight: 67,
+                height: 181
+            }
+        }
+    */
+    
+Wait, WHAT?... Yes!, an object can have other object as one of its fields/properties.
 
 ## Data Structures
 
--- lesson_2-Objects.js
+++ lesson_2-Objects.js
@@ -5,7 +5,7 @@
  * In Javascript, everything else is an Object!
  ****************************************************************************/
 
-export let naruto = {
+let naruto = {
   name:     'Naruto',
   lastName: 'Uzumaki',
   age:      12,
@@ -13,7 +13,7 @@ export let naruto = {
   height:   145
 };
 
-export let sakura = {
+let sakura = {
   name:     'Sakura',
   lastName: 'Hanuro',
   age:      12,
@@ -21,7 +21,7 @@ export let sakura = {
   height:   148
 };
 
-export let sasuke = {
+let sasuke = {
   name:     'Sasuke',
   lastName: 'Uchiha',
   age:      12,
@@ -29,7 +29,7 @@ export let sasuke = {
   height:   150
 };
 
-export let kakashi = {
+let kakashi = {
   name:     'Kakashi',
   lastName: 'Hatake',
   age:      26,
@@ -37,13 +37,63 @@ export let kakashi = {
   height:   181
 };
 
-/*
+
+/****************************************************************************
+ * An object doesn't necessarily need to be created following the structure
+ * shown above. There are other ways to define an object.
+ * Here are a couple more ways to do it!
+ ****************************************************************************/
+
 let ninja = new Object();
 
 ninja.name      = 'Naruto';
 ninja.lastName  = 'Uzumaki';
-ninja.aldea     = 'Konoha';
 ninja.age       = 12;
 ninja.weight    = 40;
 ninja.height    = 145;
-*/
+
+console.log(ninja);
+
+function Ninja(name, lastName, age, weight, height) {
+  this.name     = name;
+  this.lastName = lastName;
+  this.age      = age;
+  this.weight   = weight;
+  this.height   = height
+}
+
+ninja = new Ninja('Naruto', 'Uzumaki', 12, 40, 145);
+
+console.log(ninja);
+
+
+/****************************************************************************
+ * Working with objects!
+ *
+ * As we said before, the advantage of using an object, is to encapsulate a
+ * certain group of properties that are unique from the rest. We can access
+ * each and one of these properties like this:
+ ****************************************************************************/
+
+let age      = ninja.age;
+let name     = ninja.name;
+let lastName = ninja.lastName;
+
+console.log("Our ninja is " + age + " year old, and his name is " + name + " " + lastName);
+
+
+/****************************************************************************
+ * And we don't have to settle with the properties that the object initially
+ * has. We can dynamically add more fields to an object upon our code
+ * execution!
+ ****************************************************************************/
+
+ninja.village = "Konoha";
+ninja.sensei  = kakashi;
+
+console.log(ninja);
+
+/****************************************************************************
+ * Wait, WHAT?... Yes!, an object can have other object as one of
+ * its fields/properties.
+ ****************************************************************************/
